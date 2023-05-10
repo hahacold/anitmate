ab275b8d39b5efb380e6fd98797cff1f6afb2b7c
hank <taiwanhank0908@gmail.com>
2023-05-10 11:07:23 +0800

    Initial Commit

---
+++ anitmate/anitmateApp.swift
@@ -0,0 +1,17 @@
+//
+//  anitmateApp.swift
+//  anitmate
+//
+//  Created by User19 on 2023/5/10.
+//
+
+import SwiftUI
+
+@main
+struct anitmateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
