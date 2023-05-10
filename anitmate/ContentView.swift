e31fcaf6a753690d07930f45e38a9afeb9281455
hank <taiwanhank0908@gmail.com>
2023-05-10 12:44:10 +0800

    動畫＋動畫時禁止操作

--- anitmate/ContentView.swift
+++ anitmate/ContentView.swift
@@ -8,12 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var offset = CGSize.zero
+    @State var disable = false
+    let duration: Double = 2
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+        
+        VStack {
+            Button("HAHA"){
+                
+                withAnimation(Animation.easeInOut(duration: duration)) {
+                    if(disable==false){
+                        disable=true
+                        offset.height += 100
+                        offset.width += 100
+                    
+                    }
+                    
+                    
+                }
+                DispatchQueue.main.asyncAfter(deadline: .now()+duration){
+                    disable=false;
+                }
+            }.offset(offset)
+            Image(systemName: "applelogo")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 100, height: 100)
+                .offset(offset)
+                .onTapGesture {
+                
+                    withAnimation(Animation.easeInOut(duration: duration)) {
+                        if(disable==false){
+                            disable=true
+                            offset.height += 100
+                            offset.width += 100
+                        
+                        }
+                        
+                    }
+                    DispatchQueue.main.asyncAfter(deadline: .now()+duration){
+                        disable=false;
+                    }
+                    
+                }
+        }
+        
+        }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
